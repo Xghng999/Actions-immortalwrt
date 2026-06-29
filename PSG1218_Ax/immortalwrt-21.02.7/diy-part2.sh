7576342da30f2fb15fe1c72a569279b4c2745aa4
Xghng999 <1369492@qq.com>
2026-06-29 15:58:08 +0800

    Update diy-part2.sh

--- PSG1218_Ax/immortalwrt-21.02.7/diy-part2.sh
+++ PSG1218_Ax/immortalwrt-21.02.7/diy-part2.sh
@@ -18,4 +18,4 @@ sed -i 's/192.168.1.1/192.168.5.222/g' package/base-files/files/bin/config_gener
 # git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/lean/luci-theme-opentomcat
 
 # 去除默认主题
-sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+# sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
