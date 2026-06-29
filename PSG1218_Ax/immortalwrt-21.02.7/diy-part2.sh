cc896e36e01ca6a7867c48ea9fa350ec09ff221c
Xghng999 <1369492@qq.com>
2026-06-29 16:01:45 +0800

    Update diy-part2.sh

--- PSG1218_Ax/immortalwrt-21.02.7/diy-part2.sh
+++ PSG1218_Ax/immortalwrt-21.02.7/diy-part2.sh
@@ -6,7 +6,7 @@
 # Blog: https://p3terx.com
 #=================================================
 # 修改机器名称
-#sed -i "s/OpenWrt/PSG1218/g" package/base-files/files/bin/config_generate
+sed -i "s/OpenWrt/Phicomm_PSG1218_Ax/g" package/base-files/files/bin/config_generate
 
 # 修改设置lan ip
 sed -i 's/192.168.1.1/192.168.128.1/g' package/base-files/files/bin/config_generate
